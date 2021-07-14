60a4a0615b45f77bc980aeba6032f19b7f48053e
ATMATEC <62761888+ATMATEC@users.noreply.github.com>
2021-07-14 15:15:26 -0300

    Transições no header e no toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'danilo-chaveiro';
+
+
+  @HostListener('window:scroll', ['$event'])
+  public onScroll(){
+    const windowTop = window.pageYOffset// + ((window.innerHeight * 3.2) / 8);
+    if(windowTop > 0){
+      document.getElementById("app").style.opacity = "0%";
+      document.getElementById("initial").style.backgroundColor = "#ffff66";
+      document.getElementById("initial").style.position = "fixed"
+      document.getElementById("initial").style.top = "0"
+      document.getElementById("initial").style.zIndex = "100"
+    } else {
+      document.getElementById("app").style.opacity = "100%";
+      document.getElementById("initial").style.backgroundColor = "white";
+      document.getElementById("initial").style.position = "static"
+    //  document.getElementById("initial").style.top = "0"
+    }
+  }
 }
