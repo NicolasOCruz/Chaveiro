385725a5fa807655285b916505571d56cfaf977f
ATMATEC <62761888+ATMATEC@users.noreply.github.com>
2021-07-14 18:33:36 -0300

    Ajuste animações menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,15 +14,17 @@ export class AppComponent {
     const windowTop = window.pageYOffset// + ((window.innerHeight * 3.2) / 8);
     if(windowTop > 0){
       document.getElementById("app").style.opacity = "0%";
+      document.getElementById("app").style.transform = "translateY(-100px)"
       document.getElementById("initial").style.backgroundColor = "#ffff66";
       document.getElementById("initial").style.position = "fixed"
-      document.getElementById("initial").style.top = "0"
+      document.getElementById("initial").style.transform = "translateY(-100px)"
       document.getElementById("initial").style.zIndex = "100"
     } else {
       document.getElementById("app").style.opacity = "100%";
+      document.getElementById("app").style.transform = "translateY(0px)"
       document.getElementById("initial").style.backgroundColor = "white";
       document.getElementById("initial").style.position = "static"
-    //  document.getElementById("initial").style.top = "0"
+      document.getElementById("initial").style.transform = "translateY(0px)"
     }
   }
 }
