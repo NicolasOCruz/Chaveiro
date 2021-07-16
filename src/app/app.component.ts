fe269b5e5423a8e5cf619b95a77f5b5f40dc2bce
Nicolas Pereira da Cruz <nicolas.cruz@atmatec.com.br>
2021-07-16 20:52:03 -0300

    Ajuste responsividade menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, HostListener } from '@angular/core';
+import { ChangeDetectorRef, Component, HostListener, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout'
 
 @Component({
   selector: 'app-root',
@@ -6,17 +7,42 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'danilo-chaveiro';
+  
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addEventListener('change', this._mobileQueryListener)
+   }
+
+   ngOnDestroy(): void{
+    this.mobileQuery.removeEventListener('change', this._mobileQueryListener)
+  }
+
+  private _mobileQueryListener!: () => void;
+  mobileQuery!: MediaQueryList;
 
 
   @HostListener('window:scroll', ['$event'])
   public onScroll(){
+
     const windowTop = window.pageYOffset// + ((window.innerHeight * 3.2) / 8);
+
+    var windowWidth = window.innerWidth;
+    var screenHeight = screen.width;
+
     if(windowTop > 0){
       document.getElementById("app").style.opacity = "0%";
       document.getElementById("app").style.transform = "translateY(-100px)"
       document.getElementById("initial").style.backgroundColor = "#ffff66";
-      document.getElementById("initial").style.transform = "translateY(-100px)"
+
+      if(windowWidth >= 280 && windowWidth <= 300)
+        document.getElementById("initial").style.transform = "translateY(-46px)"
+      else if(windowWidth > 300 && windowWidth <= 400)
+        document.getElementById("initial").style.transform = "translateY(-64px)"
+      else if(windowWidth > 400 && windowWidth <= 550)
+        document.getElementById("initial").style.transform = "translateY(-77px)"
+      else if(windowWidth > 550)
+      document.getElementById("initial").style.transform = "translateY(-96px)"
     } else {
       document.getElementById("app").style.opacity = "100%";
       document.getElementById("app").style.transform = "translateY(0px)"
